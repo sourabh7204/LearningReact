9204ad8be90269693157d171b6cc5b0b102159e4
sourabh7204 <sourabh722004@gmail.com>
2025-08-04 21:21:59 +0530

    Implemented Events

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,10 +2,21 @@ function printHello() {
   return console.log("Hello!");
 }
 
+function handleHover() {
+  return console.log("Hover over me!");
+}
+
 export default function Button() {
   return (
     <div>
       <button onClick={printHello}>Click me!</button>
+      <p onMouseOver={handleHover}>
+        When you use our services, you’re trusting us with your information. We
+        understand this is a big responsibility and work hard to protect your
+        information and put you in control. This Privacy Policy is meant to help
+        you understand what information we collect, why we collect it, and how
+        you can update, manage, export, and delete your information.
+      </p>
     </div>
   );
 }
