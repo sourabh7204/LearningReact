5a738550884cf208aea6b0e18fcbad9bd79fd3f9
sourabh7204 <sourabh722004@gmail.com>
2025-08-07 22:00:35 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import ProductTab from "./ProductTab.jsx";
-import Button from "./Button.jsx";
+// import Button from "./Button.jsx";
 import Form from "./Form.jsx";
 
 function App() {
