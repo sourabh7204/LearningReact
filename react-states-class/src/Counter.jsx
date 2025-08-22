4888224ca2c3fe913b30fd4751b8a3a16a8763fe
sourabh7204 <sourabh722004@gmail.com>
2025-08-22 23:24:15 +0530

    Initial Commit

--- react-states-class/src/Counter.jsx
+++ react-states-class/src/Counter.jsx
@@ -3,15 +3,22 @@ import { useState } from "react";
 export default function Counter() {
   let [count, setCount] = useState(0);
 
-  let inCount = () => {
-    setCount(count + 1);
-    console.log(count);
+  let incCount = () => {
+    setCount((currCount) => {
+      return currCount + 5;
+    });
+    setCount((currCount) => {
+      return currCount + 5;
+    });
+    setCount((currCount) => {
+      return currCount + 5;
+    });
   };
 
   return (
     <div>
       <h3>Count={count}</h3>
-      <button onClick={inCount}>Increse Count</button>
+      <button onClick={incCount}>Increse Count</button>
     </div>
   );
 }
---
+++ react-states-class2/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LudoBoard from "./LudoBoard";
+
+function App() {
+  return (
+    <div>
+      <LudoBoard />
+    </div>
+  );
+}
+
+export default App;
