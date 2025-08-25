8e209c747eab14d0d29808f3ce8cb69fd8e7f17b
sourabh7204 <sourabh722004@gmail.com>
2025-08-25 22:00:42 +0530

    Ticket Component

--- react-states-class2/src/App.jsx
+++ react-states-class2/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Lottery from "./Lottery";
 import LudoBoard from "./LudoBoard";
 import TodoList from "./TodoList";
+import Ticket from "./Ticket";
 
 function App() {
   return (
     <div>
-      <Lottery />
+      <Ticket ticket={[0, 1, 2]} />
+      <Ticket ticket={[5, 2, 1, 0, 8]} />
     </div>
   );
 }
